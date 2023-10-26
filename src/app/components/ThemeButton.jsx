0d695ca23987d7c7f64c3b0c8c3ea2dae5e2655e
Ayushi <ayushimanoj1201@gmail.com>
2023-10-26 13:36:02 +0530

    next-themes practice in client component

---
+++ src/app/components/ThemeButton.jsx
@@ -0,0 +1,26 @@
+'use client'
+import React, { useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
+import {SunIcon, MoonIcon} from '@heroicons/react/24/solid'
+
+export default function ThemeButton() {
+    const {resolvedTheme, setTheme} = useTheme();
+    const [mounted, setMounted] = useState(false);
+    useEffect(()=>setMounted(true),[]);
+
+    if(!mounted) return null;
+    
+  return (
+    <button 
+        aria-label='Toggle Dark Mode'
+        type='button'
+        className='flex items-center justify-center rounded-lg p-2 hover:bg-gray-300'
+        onClick={()=>
+            setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}>
+        {resolvedTheme === 'dark' ? 
+            <SunIcon className='h-5 w-5 text-orange-300'/> :
+            <MoonIcon className='h-5 w-5 text-slate-800'/>
+        }
+    </button>
+  )
+}
